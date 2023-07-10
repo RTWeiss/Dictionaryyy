cb984bb89038203bb5f5ad4e7bfb86e44de548a3
RTWeiss <ricky@rtweiss.com>
2023-07-09 20:53:33 -0400

    d

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const ejs = require("ejs");
 const fs = require("fs");
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(publicPath));
 require("dotenv").config();
 const { parse } = require("pg-connection-string");
 const { Pool } = require("pg");
