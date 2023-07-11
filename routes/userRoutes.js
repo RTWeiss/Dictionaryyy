7afa5b46727425b5462a470f7e3c1187e0ba5851
RTWeiss <ricky@rtweiss.com>
2023-07-10 23:58:59 -0400

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Pool } = require("pg");
 const bcrypt = require("bcryptjs");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
