983956b7831206624d0ebd79771da63e677f63a9
RTWeiss <ricky@rtweiss.com>
2023-07-10 23:58:43 -0400

    routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const { Pool } = require("pg");
+const bcrypt = require("bcryptjs");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 // User Registration
 router.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = new User({ username, password });
-    await user.save();
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await pool.query(
+      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *",
+      [username, hashedPassword]
+    );
 
     res.status(201).json({
       message: "User registered successfully!",
+      user: newUser.rows[0],
     });
   } catch (err) {
     res.status(500).json({ error: "There was a server side error" });
@@ -21,18 +35,27 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
+    const user = await pool.query("SELECT * FROM users WHERE username = $1", [
+      username,
+    ]);
 
-    if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).json({ error: "Invalid username or password" });
+    if (user.rows.length > 0) {
+      const validPassword = await bcrypt.compare(
+        password,
+        user.rows[0].password
+      );
+
+      if (!validPassword) {
+        return res.status(401).json({ error: "Incorrect password!" });
+      }
+
+      res.json({
+        message: "Logged in successfully!",
+        user: user.rows[0],
+      });
+    } else {
+      res.status(401).json({ error: "User does not exist!" });
     }
-
-    // Session creation, token generation etc.
-    // For simplicity, it is not included here
-
-    res.json({
-      message: "User logged in successfully!",
-    });
   } catch (err) {
     res.status(500).json({ error: "There was a server side error" });
   }
